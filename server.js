ca15ff2a11e054d32c6276451b94b9f9c1e5d360
Jerameel <amilcruise@yahoo.com>
2017-09-05 22:32:18 +1000

    removed setInterval

--- server.js
+++ server.js
@@ -133,8 +133,8 @@ wss.on('connection', function connection(ws) {
   );
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
