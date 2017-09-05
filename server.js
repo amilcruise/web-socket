439a9be856feb841b78b97f19c8fe31638aeb25d
Jerameel <amilcruise@yahoo.com>
2017-09-05 22:27:57 +1000

    update server.js

--- server.js
+++ server.js
@@ -80,9 +80,26 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
+var url = require('url');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
+
+var state = {
+  "state": {
+    "fragment": {
+      "fragments": {}
+    },
+    "route": {
+      "slide": 0,
+      "params": []
+    },
+    "style": {
+      "globalStyleSet": true
+    }
+  },
+  "type": "REMOTE_STATE"
+};
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
@@ -90,9 +107,30 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+// wss.on('connection', (ws) => {
+//   console.log('Client connected');
+//   ws.on('close', () => console.log('Client disconnected'));
+// });
+
+wss.on('connection', function connection(ws) {
+  const location = url.parse(ws.upgradeReq.url, true);
+  // you might use location.query.access_token to authenticate or share sessions
+  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+  ws.on('message', function incoming(message) {
+    const payload = JSON.parse(message);
+    state = payload;
+    wss.clients.forEach(function (client) {
+      if (client !== ws) client.send(message);
+    });
+  });
+
+  ws.on('close', function close() {
+    console.log('socket disconnected');
+  });
+
+  ws.send(
+    JSON.stringify(state)
+  );
 });
 
 setInterval(() => {
